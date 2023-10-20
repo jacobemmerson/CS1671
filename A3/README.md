543f0638200c8d5228b69c8f9720377d98e1a648
jacob <emmerson.jacob@gmail.com>
2023-10-20 19:26:30 -0400

    done

---
+++ A3/README.md
@@ -0,0 +1,22 @@
+# CS1671 (Undergraduate) Assignment 3
+Jacob Emmerson
+
+Due: 10/20/23 at 11:59pm
+
+### About
+
+This script was implemented in Python 3.11.4 utilizng WSL with an Ubuntu distribution of linux.
+
+Packages Used:
+- NumPy 1.24.3
+- Scikit-learn 1.3.0
+- Pandas 2.03
+
+### Running the Code
+
+All necessary methods for utilizng the classifiers are shown in the A3_report.ipynb file. To utilize the methods in a script or notebook, place the following at the top of the file:
+
+    import hw3_skeleton *
+
+## Additional Resources Used
+- https://en.wikipedia.org/wiki/Sensitivity_and_specificity
--- A3/hw3_skeleton.py
+++ A3/hw3_skeleton.py
@@ -7,7 +7,12 @@
 #############################################################
 
 from collections import defaultdict
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
 import gzip
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 #### 1. Evaluation Metrics ####
 
@@ -16,21 +21,68 @@ import gzip
 
 ## Calculates the precision of the predicted labels
 def get_precision(y_pred, y_true):
-    ## YOUR CODE HERE...
+    tp = 0
+    fp = 0
+    for p in range(len(y_pred)): # len(pred) == len(true); p = prediction index
+        if y_pred[p] == 1: # is it PREDICTED positive
+            if y_true[p] == 1: # correct prediction
+                tp += 1
+            else:
+                fp += 1
 
-    return precision
+    return (tp/(tp+fp))
     
 ## Calculates the recall of the predicted labels
 def get_recall(y_pred, y_true):
-    ## YOUR CODE HERE...
+    tp = 0
+    fn = 0
 
-    return recall
+    for p in range(len(y_true)):
+        if y_true[p] == 1: # check all positive samples
+            if y_pred[p] == 1: # correct prediction
+                tp += 1
+            else:
+                fn += 1
+    return (tp/(tp+fn)) # recall == sensitivity
 
 ## Calculates the f-score of the predicted labels
 def get_fscore(y_pred, y_true):
-    ## YOUR CODE HERE...
+    p = get_precision(y_pred, y_true)
+    r = get_recall(y_pred, y_true)
 
-    return fscore
+    fscore = 2 * ((p * r)/(p + r))
+
+    return fscore, (p,r)
+
+def test_predictions(y_pred, y_true):
+
+    f,_ = get_fscore(y_pred, y_true)
+    p = get_precision(y_pred, y_true)
+    r = get_recall(y_pred, y_true)
+
+    print(f"Precision = {p}")
+    print(f"Recall = {r}")
+    print(f"F-Score = {f}")
+
+    return (p,r,f) # returns a tuple of precision, recall, and fscore
+
+### for error analysis
+def get_bad_preds(words, y_pred, y_true):
+    df = pd.DataFrame({'word' : words, 'y_pred' : y_pred, 'y_true' : y_true})
+    wrong = df[df.y_pred != df.y_true]
+    return wrong['word']
+
+### for plotting pr curves
+def plot_pr_curve(dict, title):
+    df = pd.DataFrame(dict).set_index('t')
+    plt.title("Precision-Recall Curve for {}".format(title))
+    plt.plot(df['r'], df['p'], 'blue', marker = 'o', linewidth = 1.2)
+    plt.plot([1,0], [0,1], 'r--', linewidth = 1, alpha = 0.5)
+    plt.xlim([-0.05,1.05])
+    plt.ylim([-0.05,1.05])
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.show()
 
 #### 2. Complex Word Identification ####
 
@@ -52,25 +104,79 @@ def load_file(data_file):
 
 ## Makes feature matrix for all complex
 def all_complex_feature(words):
+    return [words,[1] * len(words)]
 
 ## Labels every word complex
 def all_complex(data_file):
-    ## YOUR CODE HERE...
-    performance = [precision, recall, fscore]
-    return performance
+    data = load_file(data_file)
+    words = data[0] # data is a tuple where data_file[0] are the words and [1] are the labels
+    true_labels = data[1]
+    preds = all_complex_feature(words)[1]
+    p,r,f = test_predictions(preds, true_labels)
 
+    performance = [p, r, f]
+    return performance
 
 ### 2.2: Word length thresholding
 
 ## Makes feature matrix for word_length_threshold
 def length_threshold_feature(words, threshold):
+    preds = []
+    for w in words:
+        if len(w) >= threshold:
+            preds.append(1)
+        else:
+            preds.append(0)
+    return [words,preds]
 
 ## Finds the best length threshold by f-score, and uses this threshold to
 ## classify the training and development set
 def word_length_threshold(training_file, development_file):
-    ## YOUR CODE HERE
-    training_performance = [tprecision, trecall, tfscore]
-    development_performance = [dprecision, drecall, dfscore]
+    thresh = 0
+    t_data = load_file(training_file)
+    t_words = t_data[0]
+    t_labels = t_data[1]
+
+    d_data = load_file(development_file)
+    d_words = d_data[0]
+    d_labels = d_data[1]
+
+    best_f = 0
+    pr_dict = {
+        't' : [],
+        'p' : [],
+        'r' : []
+        }
+    for t in range(21): # try thresh [0,20]
+        temp = length_threshold_feature(t_words, t)
+        f,_pr = get_fscore(temp[1], t_labels)
+        if f > best_f:
+            best_f = f
+            thresh = t
+
+        # for plotting pr curve
+        pr_dict['t'].append(t)
+        pr_dict['p'].append(_pr[0])
+        pr_dict['r'].append(_pr[1])
+        
+
+    print(f"Best Threshold Found = {thresh}")
+    print('-' * 20)
+    
+    t_M = length_threshold_feature(t_words, thresh)
+    d_M = length_threshold_feature(d_words, thresh)
+
+    print("Training Scores:")
+    tp, tr, tf = test_predictions(t_M[1], t_labels)
+
+    print("\nDevelopment Scores:")
+    dp, dr, df = test_predictions(d_M[1], d_labels)
+
+    training_performance = [tp, tr, tf]
+    development_performance = [dp, dr, df]
+
+    plot_pr_curve(pr_dict, title = "Different Word Length Thresholds")
+
     return training_performance, development_performance
 
 ### 2.3: Word frequency thresholding
@@ -90,32 +196,260 @@ def load_ngram_counts(ngram_counts_file):
 
 ## Make feature matrix for word_frequency_threshold
 def frequency_threshold_feature(words, threshold, counts):
+    preds = []
+    for w in words:
+        try: # catch unseen words
+            freq = counts[w]
+        except:
+            freq = 0 
+
+        if freq <= threshold: # if word is not frequently seen, it is complex
+            preds.append(1)
+        else:
+            preds.append(0)
+
+    return [words,preds]
 
 def word_frequency_threshold(training_file, development_file, counts):
-    ## YOUR CODE HERE
-    training_performance = [tprecision, trecall, tfscore]
-    development_performance = [dprecision, drecall, dfscore]
+    thresh = 0
+    t_data = load_file(training_file)
+    t_words = t_data[0]
+    t_labels = t_data[1]
+
+    d_data = load_file(development_file)
+    d_words = d_data[0]
+    d_labels = d_data[1]
+
+    # bounds for threshold optimization
+    l_B = min(counts.values())
+    u_B = max(counts.values()) // 2 # divison by 2 is optional (a thresh = upper bound would be equal to first baseline)
+
+    print(f"Trying Thresholds between {[l_B, u_B]}")
+
+    best_f = 0
+    pr_dict = {
+        't' : [],
+        'p' : [],
+        'r' : []
+    }
+    for t in np.linspace(l_B, u_B, 50000): # try 10000 thresholds
+        temp = frequency_threshold_feature(t_words, threshold = t, counts = counts)
+        f,_pr = get_fscore(temp[1], t_labels) 
+        if f > best_f:
+            best_f = f
+            thresh = t
+        #else: break # quick stopping, assumes local max = global max
+
+        pr_dict['t'].append(t)
+        pr_dict['p'].append(_pr[0])
+        pr_dict['r'].append(_pr[1])
+
+    print(f"Best Threshold Found = {thresh}")
+    print('-' * 20)
+    t_M = frequency_threshold_feature(t_words, threshold = thresh, counts = counts)
+    d_M = frequency_threshold_feature(d_words, threshold = thresh, counts = counts)
+
+    print("Training Scores:")
+    tp, tr, tf = test_predictions(t_M[1], t_labels)
+
+    print("\nDevelopment Scores:")
+    dp, dr, df = test_predictions(d_M[1], d_labels)
+
+    training_performance = [tp, tr, tf]
+    development_performance = [dp, dr, df]
+
+    plot_pr_curve(pr_dict, title = "Different Word Frequency Thresholds")
+
     return training_performance, development_performance
 
 ### 2.4: Naive Bayes
+
+def get_clf_features(DataFrame, counts, standardize = True, std_args = {'mu' : -1, 'sd' : 0}):
+    df = DataFrame
+    df['length'] = [len(w) for w in df['words']] # length of each word
+    df['freq'] = [counts[w] for w in df['words']] # frequency according to counts
+    
+    mu_sd = 0
+    if standardize:
+        try:
+            if std_args['mu'] == -1:
+                mu = df[['length','freq']].mean()
+                sd = df[['length', 'freq']].std()
+        except:
+            mu = std_args['mu']
+            sd = std_args['sd']
+
+        df[['length', 'freq']] = (df[['length', 'freq']] - mu) / sd
+
+        mu_sd = (mu,sd)
+
+    return df, mu_sd
         
 ## Trains a Naive Bayes classifier using length and frequency features
 def naive_bayes(training_file, development_file, counts):
-    ## YOUR CODE HERE
-    training_performance = (tprecision, trecall, tfscore)
-    development_performance = (dprecision, drecall, dfscore)
-    return development_performance
+    
+    ### load data
+    t_data = load_file(training_file)
+    t_words = t_data[0]
+    t_labels = t_data[1]
+
+    d_data = load_file(development_file)
+    d_words = d_data[0]
+    d_labels = d_data[1]
+
+    ### make dataframes and standardize (if standardize = true)
+    train_df = pd.DataFrame({'words' : t_words})
+    train_df, t_ms = get_clf_features(train_df, counts)
+
+    dev_df = pd.DataFrame({'words' : d_words})
+    dev_df, d_ms = get_clf_features(dev_df, counts, std_args={'mu' : t_ms[0], 'sd' : t_ms[1]})
+
+    ### fit classifier
+    x_M = train_df[['length','freq']].to_numpy() #[word length, word frequency] features
+    y_M = np.array(t_labels) # 1D array of labels
+
+    smoother = 0
+    best_f = 0
+    pr_dict = {
+        't' : [],
+        'p' : [],
+        'r' : []
+        }
+    for e in np.logspace(1,-9,100):
+        clf = GaussianNB(var_smoothing = e)
+        clf.fit(x_M, y_M)
+        t_preds = clf.predict(x_M)
+        f,_pr = get_fscore(t_preds, t_labels)
+
+        if f > best_f:
+            best_f = f
+            smoother = e
+
+        # pr curve
+        pr_dict['t'].append(e)
+        pr_dict['p'].append(_pr[0])
+        pr_dict['r'].append(_pr[1])
+
+    print(f"Best Smoother Found = {smoother}")
+    print('-' * 20)
+
+    clf = GaussianNB(var_smoothing = smoother)
+    clf.fit(x_M, y_M)
+
+    ### predictions
+    test_M = dev_df[['length','freq']].to_numpy()
+    t_preds = clf.predict(x_M)
+    d_preds = clf.predict(test_M)
+
+    if 0: # report matrices if 1
+        print(f"{x_M = }")
+        print(f"{y_M = }")
+        print(f"{test_M = }")
+
+    print("\nTraining Scores:")
+    tp, tr, tf = test_predictions(t_preds, t_labels)
+
+    print("\nDevelopment Scores:")
+    dp, dr, df = test_predictions(d_preds, d_labels)
+
+    wrong_train_preds = get_bad_preds(words = t_words, y_pred = t_preds, y_true = t_labels)
+    wrong_dev_preds = get_bad_preds(words = d_words, y_pred = d_preds, y_true = d_labels)
+
+    training_performance = (tp, tr, tf)
+    development_performance = (dp, dr, df)
+
+    plot_pr_curve(pr_dict, "Naive Bayes across Different Add-Epsilon Smoothing")
+
+    return development_performance, {"train" : wrong_train_preds, "dev" : wrong_dev_preds}, clf
 
 ### 2.5: Logistic Regression
 
-## Trains a Naive Bayes classifier using length and frequency features
 def logistic_regression(training_file, development_file, counts):
-    ## YOUR CODE HERE    
-    training_performance = (tprecision, trecall, tfscore)
-    development_performance = (dprecision, drecall, dfscore)
-    return development_performance
+    ### load data
+    t_data = load_file(training_file)
+    t_words = t_data[0]
+    t_labels = t_data[1]
 
+    d_data = load_file(development_file)
+    d_words = d_data[0]
+    d_labels = d_data[1]
 
+    ### make dataframes
+    train_df = pd.DataFrame({'words' : t_words})
+    train_df, t_ms = get_clf_features(train_df, counts)
+
+    dev_df = pd.DataFrame({'words' : d_words})
+    dev_df, d_ms = get_clf_features(dev_df, counts, std_args={'mu' : t_ms[0], 'sd' : t_ms[1]})
+
+    ### fit classifier
+    # penalty = l2 = ridge regression w/ default strength
+    x_M = train_df[['length','freq']].to_numpy() #[word length, word frequency] features
+    y_M = np.array(t_labels) # 1D array of labels
+
+    reg_strength = 0
+    best_f = 0
+    pr_dict = {
+        't' : [],
+        'p' : [],
+        'r' : []
+        }
+    for c in np.logspace(4,-4, 100):
+        clf = LogisticRegression(
+            penalty = 'l2',
+            solver = 'lbfgs', 
+            max_iter = 100, 
+            n_jobs = 2,
+            C = c
+            )
+        clf.fit(x_M, y_M)
+        t_preds = clf.predict(x_M)
+        f,_pr = get_fscore(t_preds, t_labels)
+
+        if f > best_f:
+            best_f = f
+            reg_strength = c
+
+        # pr curve
+        pr_dict['t'].append(c)
+        pr_dict['p'].append(_pr[0])
+        pr_dict['r'].append(_pr[1])
+
+    print(f"Best C (regularization strength) Found = {reg_strength}")
+    print('-' * 20)
+
+    clf = LogisticRegression(
+        penalty = 'l2',
+        solver = 'lbfgs', 
+        max_iter = 100, 
+        n_jobs = 2,
+        C = reg_strength
+        )
+    clf.fit(x_M, y_M) # refit on full matrix
+    print(f"coefficients for (length, freq) = {clf.coef_}")
+
+    ### predictions
+    test_M = dev_df[['length','freq']].to_numpy()
+    t_preds = clf.predict(x_M)
+    d_preds = clf.predict(test_M)
+
+    if 0: # report matrices if 1
+        print(f"{x_M = }")
+        print(f"{y_M = }")
+        print(f"{test_M = }")
+
+    print("\nTraining Scores:")
+    tp, tr, tf = test_predictions(t_preds, t_labels)
+
+    print("\nDevelopment Scores:")
+    dp, dr, df = test_predictions(d_preds, d_labels)
+
+    wrong_train_preds = get_bad_preds(words = t_words, y_pred = t_preds, y_true = t_labels)
+    wrong_dev_preds = get_bad_preds(words = d_words, y_pred = d_preds, y_true = d_labels)
+
+    plot_pr_curve(pr_dict, "Logistic Regression across Different Regularization Strengths")
+
+    development_performance = (dp, dr, df)
+    return development_performance, {"train" : wrong_train_preds, "dev" : wrong_dev_preds}, clf
 
 if __name__ == "__main__":
     training_file = "data/complex_words_training.txt"
