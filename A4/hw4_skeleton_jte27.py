d4a0d4fe5966c604a93f139eae86271f30c672d2
jacob <emmerson.jacob@gmail.com>
2023-11-19 17:17:21 -0500

    done

--- A4/hw4_skeleton_jte27.py
+++ A4/hw4_skeleton_jte27.py
@@ -4,7 +4,23 @@ import csv
 import re
 import random
 import numpy as np
+from scipy.sparse import csr_matrix
 
+def subset_data(linetuples, documentnames, ndocs, random = False):
+    if random:
+        doc_subset = np.random.choice(documentnames, size = ndocs, replace = False)
+    else:
+        doc_subset = documentnames[:ndocs]
+
+    lt_subset = []
+    vocab_subset = set()
+
+    for line in linetuples:
+        if line[0] in doc_subset:
+            lt_subset.append(line)
+            vocab_subset = vocab_subset.union(set(line[1])) # add new words to vocab
+
+    return lt_subset,doc_subset,list(vocab_subset)
 
 def read_in_shakespeare():
     """Reads in the Shakespeare dataset processesit into a list of tuples.
@@ -40,33 +56,6 @@ def read_in_shakespeare():
 
     return tuples, document_names, vocab
 
-
-def get_row_vector(matrix, row_id):
-    """A convenience function to get a particular row vector from a numpy matrix
-
-    Inputs:
-      matrix: a 2-dimensional numpy array
-      row_id: an integer row_index for the desired row vector
-
-    Returns:
-      1-dimensional numpy array of the row vector
-    """
-    return matrix[row_id, :]
-
-
-def get_column_vector(matrix, col_id):
-    """A convenience function to get a particular column vector from a numpy matrix
-
-    Inputs:
-      matrix: a 2-dimensional numpy array
-      col_id: an integer col_index for the desired row vector
-
-    Returns:
-      1-dimensional numpy array of the column vector
-    """
-    return matrix[:, col_id]
-
-
 def create_term_document_matrix(line_tuples, document_names, vocab):
     """Returns a numpy array containing the term document matrix for the input lines.
 
@@ -85,9 +74,21 @@ def create_term_document_matrix(line_tuples, document_names, vocab):
           and each column corresponds to a document. A_ij contains the
           frequency with which word i occurs in document j.
     """
-    # YOUR CODE HERE
+    m = len(vocab)
+    n = len(document_names)
 
-    return None
+    doc_index = dict(zip(document_names, range(len(document_names))))
+    word_index = dict(zip(vocab, range(len(vocab))))
+    td_matrix = np.zeros(shape = (m,n), dtype=np.int32)
+
+    for line in line_tuples:
+        di = doc_index[line[0]]
+        words = line[1]
+        for w in words:
+            wi = word_index[w]
+            td_matrix[wi, di] += 1
+
+    return td_matrix
 
 
 def create_term_context_matrix(line_tuples, vocab, context_window_size=1):
@@ -107,10 +108,31 @@ def create_term_context_matrix(line_tuples, vocab, context_window_size=1):
           word j was found within context_window_size to the left or right of
           word i in any sentence in the tuples.
     """
+    n = len(vocab)
+    cws = context_window_size
 
-    # YOUR CODE HERE
+    word_index = dict(zip(vocab, range(len(vocab))))
+    tc_matrix = np.zeros(shape = (n,n), dtype = np.int32)
 
-    return None
+    for line in line_tuples:
+        sentence = line[1] # don't care about documents
+
+        for i in range(len(sentence)):
+            target_index = word_index[sentence[i]] # target word (row index)
+            
+            L_win = sentence[max(0, i - cws):(i)] # upper is exclusive
+            if i == len(sentence): # if we are at the end of the sentence, no upper window 
+              #(i + 1) throws an error
+                U_win = []
+            else: 
+                U_win = sentence[(i+1):(i + cws + 1)] # don't include the target word
+
+            window = L_win + U_win
+            for word in window: # add the word instances to the tc_matrix
+                wj = word_index[word] #context index
+                tc_matrix[target_index,wj] += 1
+
+    return tc_matrix
 
 
 def create_tf_idf_matrix(term_document_matrix):
@@ -128,9 +150,15 @@ def create_tf_idf_matrix(term_document_matrix):
       A numpy array with the same dimension as term_document_matrix, where
       A_ij is weighted by the inverse document frequency of document h.
     """
-
-    # YOUR CODE HERE
-    return None
+    N = term_document_matrix.shape[1] # number of documents
+    tf = np.log(term_document_matrix + 1) # log counts of term frequencies
+    df = np.count_nonzero(term_document_matrix, axis = 1)
+    idf = np.log(N/df)
+    
+    for d in range(N):
+       tf[:,d] *= idf 
+    
+    return tf #tf-idf mat
 	
 def create_ppmi_matrix(term_context_matrix):
     """Given the term context matrix, output a ppmi weighted version.
@@ -147,9 +175,15 @@ def create_ppmi_matrix(term_context_matrix):
       A numpy array with the same dimension as term_context_matrix, where
       A_ij is weighted using PPMI.
     """
+    tcm = csr_matrix(term_context_matrix) # saving memory for calculations
+    word_counts = tcm.sum(axis = 1).reshape(-1,1) #sum across rows
+    context_counts = tcm.sum(axis = 0) #columns
+    total = tcm.sum() # total matrix counts
 
-    # YOUR CODE HERE
-    return None
+    ppmi_mat = np.log2((tcm * total) / (word_counts * context_counts))
+    ppmi_mat = np.maximum(np.nan_to_num(ppmi_mat, copy = False), 0) # replace nan and negative values with 0
+
+    return np.asarray(ppmi_mat)
 
 
 def compute_cosine_similarity(vector1, vector2):
@@ -165,9 +199,23 @@ def compute_cosine_similarity(vector1, vector2):
     Returns:
       A scalar similarity value.
     """
-    # YOUR CODE HERE
-    return None
 
+    len1 = np.sqrt(vector1.dot(vector1))
+    len2 = np.sqrt(vector2.dot(vector2))
+
+    if len1 == 0 or len2 == 0: # prevent divison by zero; caused by frequent terms using tf-idf or infrequent words in tc matrix
+        return 0
+
+    return vector1.dot(vector2)/(len1 * len2)
+
+def sort_by_column(matrix, col_index, ascending = False):
+    mat = matrix
+    if ascending:
+        a = 1
+    else:
+        a = -1
+    # uses quicksort (not stable), non issue with continuous data
+    return mat[mat[:,col_index].argsort(kind = "quicksort")[::a]]
 
 def rank_words(target_word_index, matrix):
     """Ranks the similarity of all of the words to the target word using compute_cosine_similarity.
@@ -182,54 +230,23 @@ def rank_words(target_word_index, matrix):
       A length-n list of similarity scores, ordered by decreasing similarity to the
       target word indexed by word_index
     """
-    # YOUR CODE HERE
-    return [], []
-	
+    mat = matrix
+    nrows = mat.shape[0] # number of rows  
+    woi = mat[target_word_index,:] # word of interest  
 
-if __name__ == "__main__":
-    tuples, document_names, vocab = read_in_shakespeare()
+    for r in range(nrows):
+        if r == target_word_index: # don't compute similarity with itself
+            continue
+        
+        s = compute_cosine_similarity(woi, mat[r,:])
 
-    print("Computing term document matrix...")
-    td_matrix = create_term_document_matrix(tuples, document_names, vocab)
+        # create the array on first pass
+        if r == 0:
+            sims = np.array([[r,s]])
 
-    print("Computing tf-idf matrix...")
-    tf_idf_matrix = create_tf_idf_matrix(td_matrix)
+        else:
+          sims = np.append(sims, [[r,s]], axis = 0)
+    
+    sims = sort_by_column(sims, 1)
 
-
-    print("Computing term context matrix...")
-    tc_matrix = create_term_context_matrix(tuples, vocab, context_window_size=2)
-
-    # random_idx = random.randint(0, len(document_names) - 1)
-
-    word = "juliet"
-    vocab_to_index = dict(zip(vocab, range(0, len(vocab))))
-
-    print(
-        '\nThe 10 most similar words to "%s" using cosine-similarity on term-document frequency matrix are:'
-        % (word)
-    )
-    ranks, scores = rank_words(vocab_to_index[word], td_matrix)
-    for idx in range(0,10):
-        word_id = ranks[idx]
-        print("%d: %s; %s" %(idx+1, vocab[word_id], scores[idx]))
-
-    print(
-        '\nThe 10 most similar words to "%s" using cosine-similarity on term-context frequency matrix are:'
-        % (word)
-    )
-    ranks, scores = rank_words(vocab_to_index[word], tc_matrix)
-    for idx in range(0,10):
-        word_id = ranks[idx]
-        print("%d: %s; %s" %(idx+1, vocab[word_id], scores[idx]))
-
-
-    print(
-        '\nThe 10 most similar words to "%s" using cosine-similarity on tf-idf matrix are:'
-        % (word)
-    )
-    ranks, scores = rank_words(vocab_to_index[word], tf_idf_matrix)
-    for idx in range(0,10):
-        word_id = ranks[idx]
-        print("%d: %s; %s" %(idx+1, vocab[word_id], scores[idx]))
-
-
+    return sims[:,0].astype(np.int32), sims[:,1]
